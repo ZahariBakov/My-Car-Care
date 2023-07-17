34019816078537ab6a5a8fc0f076730c66d6c178
ZahariBakov <zaharibakov@gmail.com>
2023-07-17 18:11:42 +0300

    Update Django project "my_car_care_project".

--- my_car_care_project/my_car_care_project/maintenance/urls.py
+++ my_car_care_project/my_car_care_project/maintenance/urls.py
@@ -4,4 +4,5 @@ from my_car_care_project.maintenance.views import maintenance_page
 
 urlpatterns = (
     path('', maintenance_page, name='maintenance'),
+    path('add/<int:car_id>/', MaintenanceAddView.as_view(), name='maintenance-add'),
 )
--- my_car_care_project/my_car_care_project/maintenance/views.py
+++ my_car_care_project/my_car_care_project/maintenance/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic as views
 from my_car_care_project.maintenance.models import Maintenance
 
 
@@ -6,3 +7,7 @@ def maintenance_page(request):
     maintenance = Maintenance.objects.all()
     context = {'maintenance': maintenance}
     return render(request, 'maintenance/maintenance-page.html', context)
+
+
+class MaintenanceAddView(views.CreateView):
+    pass
