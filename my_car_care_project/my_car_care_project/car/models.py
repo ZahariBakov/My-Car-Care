541f4fb5a1959e42a550847490c144b198a9c702
ZahariBakov <zaharibakov@gmail.com>
2023-07-20 22:57:05 +0300

    Update Django project "my_car_care_project".

--- my_car_care_project/my_car_care_project/car/models.py
+++ my_car_care_project/my_car_care_project/car/models.py
@@ -5,9 +5,36 @@ UserModel = get_user_model()
 
 
 class Car(models.Model):
-    brand = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
+    brand = models.CharField(
+        max_length=100
+    )
+
+    model = models.CharField(
+        max_length=100
+    )
+
     year = models.PositiveIntegerField()
+
+    odometer = models.PositiveIntegerField(
+        blank=True,
+        null=True
+    )
+
+    purchase_price = models.FloatField(
+        blank=True,
+        null=True
+    )
+
+    date_of_purchase = models.DateField(
+        blank=True,
+        null=True
+    )
+
+    photo = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to='vehicle'
+    )
 
     user = models.ForeignKey(
         UserModel,
--- my_car_care_project/my_car_care_project/settings.py
+++ my_car_care_project/my_car_care_project/settings.py
@@ -128,6 +128,11 @@ STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = (
+    BASE_DIR / 'mediafiles',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
