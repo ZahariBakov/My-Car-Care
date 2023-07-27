5e00b73d03fd5f364f7112dbe383fe90995940e4
ZahariBakov <zaharibakov@gmail.com>
2023-07-27 18:07:42 +0300

    Update Django project "my_car_care_project".

--- my_car_care_project/my_car_care_project/common/urls.py
+++ my_car_care_project/my_car_care_project/common/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from my_car_care_project.common.views import IndexView
+from my_car_care_project.common.views import IndexView, learn_more_view
 
 urlpatterns = (
     path('', IndexView.as_view(), name='index'),
+    path('learn_more/', learn_more_view, name='learn more'),
 )
--- my_car_care_project/my_car_care_project/common/views.py
+++ my_car_care_project/my_car_care_project/common/views.py
@@ -1,5 +1,10 @@
+from django.shortcuts import render
 from django.views import generic as views
 
 
 class IndexView(views.TemplateView):
     template_name = 'common/index.html'
+
+
+def learn_more_view(request):
+    return render(request, 'common/learn_more.html')
