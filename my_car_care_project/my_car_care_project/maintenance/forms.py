7f50cbff2fe71f2df6c92c77ed3c1749599298b5
ZahariBakov <zaharibakov@gmail.com>
2023-08-14 17:39:23 +0300

    Update Django project "my_car_care_project". Change date format. Add class to register button in 'register-page.html'. Create '404.html' page.

--- my_car_care_project/my_car_care_project/maintenance/forms.py
+++ my_car_care_project/my_car_care_project/maintenance/forms.py
@@ -4,8 +4,6 @@ from my_car_care_project.maintenance.models import Maintenance, Repair
 
 
 class MaintenanceAddForm(forms.ModelForm):
-    date = forms.DateField(input_formats=['%d.%m.%y'])
-
     class Meta:
         model = Maintenance
         fields = ['title', 'description', 'date', 'cost', 'status']
